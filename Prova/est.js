07926e40329af7eac8a57a89128f01eecf021b7b
JOAO VICTOR CAMPOS MONTI <05970646016@senacrs.edu.br>
2026-07-16 10:28:31 -0300

    Pronto

--- Prova/est.js
+++ Prova/est.js
@@ -8,16 +8,14 @@ for(let i = 0; i < 5; i++) {
     let carro = prompt(`Digite o ${i + 1}º carro: `)
     carros.push(carro)
 }
-console.log("\nDigite o nome de um carro para colocar no inicio da lista: ")
+
+console.log("\nDigite o nome de um carro para colocar no início da lista: ")
 let novoCarro = prompt()
+carros.unshift(novoCarro)
 
 console.log("\nDigite o nome de um carro para colocar no final da lista: ")
-let = prompt()
+novoCarro = prompt()
 carros.push(novoCarro)
 
 console.log("\nLista de carros:")
 console.log(carros)
-
-console.log("\nPrimeiro carro cadastrado: " + carros[0])
-console.log("Último carro cadastrado: " + carros[carros.length - 1])
-console.log("Quantidade de carros: " + carros.length)
